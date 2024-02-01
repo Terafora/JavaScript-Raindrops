2ecc42e8fc954d3d742c6993985dbbc2185284db
Charlotte Stone <charlie.stone649@gmail.com>
2024-02-01 09:26:17 +0000

    Add solution my solution to project

---
+++ index.js
@@ -0,0 +1,8 @@
+export const convert = (input) => {
+    let result = ""
+    if(input % 3 === 0) result += 'Pling';
+    if(input % 5 === 0) result += 'Plang';
+    if(input % 7 === 0) result += 'Plong';
+    if(input % 3 !== 0 && input % 5 !== 0 && input % 7 !== 0) result = input.toString();
+    return result;
+  };
